edb1960fc215af6f5fa8a5dbba207df4608e3f29
Vo TRong Nghiem <nghiemV2001@gmail.com>
2023-12-05 01:52:17 +0700

    24 day with design UI Flutter

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:design_ui/widget/Day4.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Day_4(),
+    ));
