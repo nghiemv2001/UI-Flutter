0e66d79ba6ff27d2584aa2056cfca7017fff96a5
Vo TRong Nghiem <nghiemV2001@gmail.com>
2023-12-25 17:34:30 +0700

    Day 7 (improve design with ListView SingleChildScrollView, Container, Text, Image)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:design_ui/widget/Day_6.dart';
+import 'package:design_ui/widget/Day7.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    theme: ThemeData(
-        primaryColor: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity),
-    debugShowCheckedModeBanner: false,
-    home: Day_6()));
+void main() => runApp(
+      MaterialApp(
+        theme: ThemeData(
+            primaryColor: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity),
+        debugShowCheckedModeBanner: false,
+        home: Day7(),
+      ),
+    );
